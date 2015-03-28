9a5e2a2aec0aed1b262b4ac9638d43aa08c7185f
Maria Kousta <koustamaria@gmail.com>
2015-03-28 14:59:56 +0200

    Prefill option fields with stored option values on page load

--- js/options.js
+++ js/options.js
@@ -1,5 +1,23 @@
 var $info_el;
 
+var getOptions = function(){
+  chrome.storage.local.get(function(data){
+    if(data.token){
+      $('#token').val(data.token)
+    }
+    if(data.channel){
+      $('#channel').val(data.channel)
+    }
+  });
+}
+
+var clearOptions = function(){
+  chrome.storage.local.clear(function(data){
+    $('#token').val('')
+    $('#channel').val('')
+  });
+}
+
 var saveOptions = function(token, channel){
   chrome.storage.local.set({
     'token': token,
@@ -30,9 +48,16 @@ var checkToken = function(token) {
 $(document).ready(function(){
   $info_el = $("#info-message");
 
+  getOptions();
+
   $("#save").click(function(e){
     e.preventDefault();
     saveOptions($("#token").val(), $("#channel").val());
+  });
+
+  $("#clear").click(function(e){
+    e.preventDefault();
+    clearOptions();
   });
 
   $("#check").click(function(e){
