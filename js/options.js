5e5d243a91f4bbfd2cc85961f87ff763f8067746
Maria Kousta <koustamaria@gmail.com>
2015-04-10 18:19:50 +0300

    Add IIFE to script files and switch to strict mode

--- js/options.js
+++ js/options.js
@@ -1,79 +1,84 @@
+;(function(window, document, $, chrome){
 
-var getOptions = function(){
-  chrome.storage.local.get(function(data){
-    if(data.token){
-      $('#token').val(data.token);
-    }
-    if(data.channel){
-      $('#channel').val(data.channel);
-    }
-  });
-};
+  'use strict';
 
-var clearOptions = function(){
-  chrome.storage.local.clear(function(){
-    $('#token').val('');
-    $('#channel').val('');
-  });
-};
-
-var saveOptions = function(token, channel, $success_el){
-  chrome.storage.local.set({
-    'token': token,
-    'channel': channel
-  }, function(){
-    $success_el.fadeIn();
-  });
-};
-
-var checkToken = function(token) {
-  return $.ajax({
-    type: 'GET',
-    url: 'https://slack.com/api/auth.test',
-    data: { token: token }
-  });
-};
-
-$(document).ready(function(){
-  var $info_el = $('#info-message');
-
-  getOptions();
-
-  $('#save').click(function(e){
-    e.preventDefault();
-    saveOptions($('#token').val(), $('#channel').val(), $(this).find('.icon'));
-  });
-
-  $('#clear').click(function(e){
-    e.preventDefault();
-    clearOptions();
-  });
-
-  $('#check').click(function(e){
-    e.preventDefault();
-    var $success_el = $(this).find('.icon');
-
-    checkToken($('#token').val()).then(function(resp){
-      if(resp.ok){
-        $success_el.fadeIn();
+  var getOptions = function(){
+    chrome.storage.local.get(function(data){
+      if(data.token){
+        $('#token').val(data.token);
       }
-      else{
-        $info_el.find('.icon').show();
-        $info_el.find('span').text(resp.error.replace(/_/g, ' '));
+      if(data.channel){
+        $('#channel').val(data.channel);
       }
-    }).fail(function(){
-      $info_el.find('span').text('network error');
     });
+  };
+
+  var clearOptions = function(){
+    chrome.storage.local.clear(function(){
+      $('#token').val('');
+      $('#channel').val('');
+    });
+  };
+
+  var saveOptions = function(token, channel, $success_el){
+    chrome.storage.local.set({
+      'token': token,
+      'channel': channel
+    }, function(){
+      $success_el.fadeIn();
+    });
+  };
+
+  var checkToken = function(token) {
+    return $.ajax({
+      type: 'GET',
+      url: 'https://slack.com/api/auth.test',
+      data: { token: token }
+    });
+  };
+
+  $(document).ready(function(){
+    var $info_el = $('#info-message');
+
+    getOptions();
+
+    $('#save').click(function(e){
+      e.preventDefault();
+      saveOptions($('#token').val(), $('#channel').val(), $(this).find('.icon'));
+    });
+
+    $('#clear').click(function(e){
+      e.preventDefault();
+      clearOptions();
+    });
+
+    $('#check').click(function(e){
+      e.preventDefault();
+      var $success_el = $(this).find('.icon');
+
+      checkToken($('#token').val()).then(function(resp){
+        if(resp.ok){
+          $success_el.fadeIn();
+        }
+        else{
+          $info_el.find('.icon').show();
+          $info_el.find('span').text(resp.error.replace(/_/g, ' '));
+        }
+      }).fail(function(){
+        $info_el.find('span').text('network error');
+      });
+    });
+
+    $('input').on('keyup', function(){
+      $('.icon-exclamation-circle, .icon-check').hide();
+      $('span').text('');
+    });
+
+    $('#help').click(function(e){
+      e.preventDefault();
+      $('#help-contents').toggle();
+    });
+
   });
 
-  $('input').on('keyup', function(){
-    $('.icon-exclamation-circle, .icon-check').hide();
-    $('span').text('');
-  });
-
-  $('#help').click(function(e){
-    e.preventDefault();
-    $('#help-contents').toggle();
-  });
-
-});
+})(window, document, jQuery, chrome);
--- js/popup.js
+++ js/popup.js
@@ -1,137 +1,143 @@
-var token;
-var defaultChannel;
-var channelNames = [];
-var groupNames = [];
+;(function(window, document, $, chrome){
 
-var getChannelsList = function(){
-  return $.ajax({
-    url: 'https://slack.com/api/channels.list',
-    data: { token: token, exclude_archived: 1 }
-  });
-};
+  'use strict';
 
-var getGroupsList = function(){
-  return $.ajax({
-    url: 'https://slack.com/api/groups.list',
-    data: { token: token, exclude_archived: 1 }
-  });
-};
+  var token;
+  var defaultChannel;
+  var channelNames = [];
+  var groupNames = [];
 
-var getAutocompleteValues = function(){
-  $.when(getChannelsList(), getGroupsList()).done(function(channelData, groupData){
-    var i;
-    if(channelData[0].ok){
-      for(i=0; i<channelData[0].channels.length; i++){
-        channelNames.push('#' + channelData[0].channels[i].name);
-      }
-    }
-    else {
-      console.log('could not get channels');
-    }
-    if(groupData[0].ok){
-      for(i=0; i<groupData[0].groups.length; i++){
-        groupNames.push(groupData[0].groups[i].name);
-      }
-    } else {
-      console.log('could not get groups');
-    }
-    $(document).trigger('gotAutompleteValues');
-  });
-};
+  var getChannelsList = function(){
+    return $.ajax({
+      url: 'https://slack.com/api/channels.list',
+      data: { token: token, exclude_archived: 1 }
+    });
+  };
 
-var sendToChannel = function(message, recipient, $success_el, $info_el){
-  chrome.tabs.query({active: true, currentWindow: true, highlighted: true}, function(tab) {
-    var url = tab[0].url;
+  var getGroupsList = function(){
+    return $.ajax({
+      url: 'https://slack.com/api/groups.list',
+      data: { token: token, exclude_archived: 1 }
+    });
+  };
 
-    var data = {
-      token: token,
-      channel: recipient,
-      text: message ? message + ' ' + url : url,
-      as_user: true
-    };
-
-    $.ajax({
-      type: 'POST',
-      url: 'https://slack.com/api/chat.postMessage',
-      data: data,
-      success: function(resp) {
-        if(resp.ok){
-         $success_el.fadeIn();
-        } else {
-          $info_el.find('.icon').show();
-          $info_el.find('span').text(resp.error.replace(/_/g, ' '));
+  var getAutocompleteValues = function(){
+    $.when(getChannelsList(), getGroupsList()).done(function(channelData, groupData){
+      var i;
+      if(channelData[0].ok){
+        for(i=0; i<channelData[0].channels.length; i++){
+          channelNames.push('#' + channelData[0].channels[i].name);
         }
-      },
-      error: function() {
-        $info_el.find('.icon').show();
-        $info_el.find('span').text('network error');
+      }
+      else {
+        console.log('could not get channels');
+      }
+      if(groupData[0].ok){
+        for(i=0; i<groupData[0].groups.length; i++){
+          groupNames.push(groupData[0].groups[i].name);
+        }
+      } else {
+        console.log('could not get groups');
+      }
+      $(document).trigger('gotAutompleteValues');
+    });
+  };
+
+  var sendToChannel = function(message, recipient, $success_el, $info_el){
+    chrome.tabs.query({active: true, currentWindow: true, highlighted: true}, function(tab) {
+      var url = tab[0].url;
+
+      var data = {
+        token: token,
+        channel: recipient,
+        text: message ? message + ' ' + url : url,
+        as_user: true
+      };
+
+      $.ajax({
+        type: 'POST',
+        url: 'https://slack.com/api/chat.postMessage',
+        data: data,
+        success: function(resp) {
+          if(resp.ok){
+           $success_el.fadeIn();
+          } else {
+            $info_el.find('.icon').show();
+            $info_el.find('span').text(resp.error.replace(/_/g, ' '));
+          }
+        },
+        error: function() {
+          $info_el.find('.icon').show();
+          $info_el.find('span').text('network error');
+        }
+      });
+    });
+  };
+
+  var registerHandlers = function(){
+
+    $('#share-with-team').click(function(e){
+      e.preventDefault();
+      if(defaultChannel){
+        sendToChannel($('#message').val(),
+          defaultChannel,
+          $(this).find('.icon'),
+          $('#team-info-message')
+        );
+      } else {
+        $('#team-info-message').find('.icon').show();
+        $('#team-info-message').find('span').text('set your team\'s channel');
       }
     });
-  });
-};
 
-var registerHandlers = function(){
-
-  $('#share-with-team').click(function(e){
-    e.preventDefault();
-    if(defaultChannel){
-      sendToChannel($('#message').val(),
-        defaultChannel,
-        $(this).find('.icon'),
-        $('#team-info-message')
-      );
-    } else {
-      $('#team-info-message').find('.icon').show();
-      $('#team-info-message').find('span').text('set your team\'s channel');
-    }
-  });
-
-  $('#share').click(function(e){
-    e.preventDefault();
-    var recipient = $('#recipient').val();
-    if(recipient.length > 0){
-      sendToChannel($('#message').val(),
-        recipient,
-        $(this).find('.icon'),
-        $('#info-message')
-      );
-    }
-    else {
-      $('#recipient').focus();
-    }
-  });
-
-  $('input').on('keyup', function(){
-    $('.icon').hide();
-    $('span').text('');
-  });
-
-  $(document).on('gotAutompleteValues', function(){
-    $('#recipient').autocomplete({
-      lookup: channelNames.concat(groupNames),
-      minChars: 2,
-      appendTo: '.suggestions',
-      lookupLimit: 12
+    $('#share').click(function(e){
+      e.preventDefault();
+      var recipient = $('#recipient').val();
+      if(recipient.length > 0){
+        sendToChannel($('#message').val(),
+          recipient,
+          $(this).find('.icon'),
+          $('#info-message')
+        );
+      }
+      else {
+        $('#recipient').focus();
+      }
     });
+
+    $('input').on('keyup', function(){
+      $('.icon').hide();
+      $('span').text('');
+    });
+
+    $(document).on('gotAutompleteValues', function(){
+      $('#recipient').autocomplete({
+        lookup: channelNames.concat(groupNames),
+        minChars: 2,
+        appendTo: '.suggestions',
+        lookupLimit: 12
+      });
+    });
+  };
+
+  $(document).ready(function() {
+
+    $('.js-options').each(function(){
+      $(this).attr('href', chrome.extension.getURL('options.html'));
+    });
+
+    chrome.storage.local.get(['token','channel'], function(data){
+      token = data.token;
+      defaultChannel = data.channel;
+      if(token!==undefined && token!==''){
+        registerHandlers();
+        getAutocompleteValues();
+      } else {
+        $('section').not('#first-time').hide();
+        $('#first-time').show();
+      }
+    });
+
   });
-};
 
-$(document).ready(function() {
-
-  $('.js-options').each(function(){
-    $(this).attr('href', chrome.extension.getURL('options.html'));
-  });
-
-  chrome.storage.local.get(['token','channel'], function(data){
-    token = data.token;
-    defaultChannel = data.channel;
-    if(token!==undefined && token!==''){
-      registerHandlers();
-      getAutocompleteValues();
-    } else {
-      $('section').not('#first-time').hide();
-      $('#first-time').show();
-    }
-  });
-
-});
+})(window, document, jQuery, chrome);
