444dc9aa57fb3133ba7ffd7533557451832d9e17
Maria Kousta <koustamaria@gmail.com>
2015-04-10 19:54:06 +0300

    Improve options js code

--- js/options.js
+++ js/options.js
@@ -2,31 +2,31 @@
 
   'use strict';
 
-  var getOptions = function(){
+  var getSavedOptions = function(){
+    var dfd = $.Deferred();
     chrome.storage.local.get(function(data){
-      if(data.token){
-        $('#token').val(data.token);
-      }
-      if(data.channel){
-        $('#channel').val(data.channel);
-      }
+      dfd.resolve(data);
     });
+    return dfd;
   };
 
   var clearOptions = function(){
+    var dfd = $.Deferred();
     chrome.storage.local.clear(function(){
-      $('#token').val('');
-      $('#channel').val('');
+      dfd.resolve();
     });
+    return dfd;
   };
 
-  var saveOptions = function(token, channel, $success_el){
+  var saveOptions = function(token, channel){
+    var dfd = $.Deferred();
     chrome.storage.local.set({
       'token': token,
       'channel': channel
     }, function(){
-      $success_el.fadeIn();
+      dfd.resolve();
     });
+    return dfd;
   };
 
   var checkToken = function(token) {
@@ -37,41 +37,67 @@
     });
   };
 
+  var humanize = function(str){
+    return str.replace(/_/g, ' ');
+  };
+
   $(document).ready(function(){
+
+    //Cache Elements
     var $info_el = $('#info-message');
+    var $info_icon = $info_el.find('.icon');
+    var $info_text = $info_el.find('span');
+    var $token_el = $('#token');
+    var $channel_el = $('#channel');
 
-    getOptions();
+    //Get saved option and show their values in input elements
+    getSavedOptions().then(function(data){
+      if(data.token){
+        $token_el.val(data.token);
+      }
+      if(data.channel){
+        $channel_el.val(data.channel);
+      }
+    });
 
+    //Register Handlers
     $('#save').click(function(e){
       e.preventDefault();
-      saveOptions($('#token').val(), $('#channel').val(), $(this).find('.icon'));
+      var $success_el = $(this).find('.icon');
+
+      saveOptions($token_el.val(), $channel_el.val()).done(function(){
+        $success_el.fadeIn();
+      });
     });
 
     $('#clear').click(function(e){
       e.preventDefault();
-      clearOptions();
+      clearOptions().then(function(){
+        $token_el.val('');
+        $channel_el.val('');
+      });
     });
 
     $('#check').click(function(e){
       e.preventDefault();
       var $success_el = $(this).find('.icon');
 
-      checkToken($('#token').val()).then(function(resp){
+      checkToken($token_el.val()).done(function(resp){
         if(resp.ok){
           $success_el.fadeIn();
         }
         else{
-          $info_el.find('.icon').show();
-          $info_el.find('span').text(resp.error.replace(/_/g, ' '));
+          $info_icon.show();
+          $info_text.text(humanize(resp.error));
         }
       }).fail(function(){
-        $info_el.find('span').text('network error');
+        $info_text.text('network error');
       });
     });
 
     $('input').on('keyup', function(){
       $('.icon-exclamation-circle, .icon-check').hide();
-      $('span').text('');
+      $info_text.text('');
     });
 
     $('#help').click(function(e){
